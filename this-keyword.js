00bd962bc3cf41ee54a57ce6ea744884b3d5ce8e
MIRJAKARIYA <mirjakariya239@gmail.com>
2023-02-11 01:27:21 +0600

    ss

--- this-keyword.js
+++ this-keyword.js
@@ -1,1 +1,24 @@
-console.log("here is the code")
+//Execution Context
+
+function a(){
+    b()
+    console.log("I am fuction A")
+}
+
+function b(){
+    d()
+    console.log("I am fuction B")
+}
+
+function c(){
+    console.log("I am function C")
+}
+
+function d(){
+    c()
+    console.log("I am fuction D")
+}
+
+var x = 100
+a()
+console.log("I am global")
